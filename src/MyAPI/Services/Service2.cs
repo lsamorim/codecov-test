4817ae9531ba37bbd2d361ef85e1f9b7b3afde22
Lucas Amorim <lsamorim@stone.com.br>
2018-03-19 19:34:52 -0300

    Add test

---
+++ src/MyAPI/Services/Service2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAPI.Services
+{
+    public class Service2
+    {
+        public void Call()
+        {
+
+        }
+    }
+}
---
+++ src/UnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Moq;
+using MyAPI.Services;
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //var serviceMock = new Mock<Service2>();
+            ////serviceMock.Setup(x => x.Call);
+
+            //var service = new Service1(serviceMock.Object);
+            //service.Call();
+            //serviceMock.Verify(x => x.Call(), Times.Once);
+        }
+    }
+}
